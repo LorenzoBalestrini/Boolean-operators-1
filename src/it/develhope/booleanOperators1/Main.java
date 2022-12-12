98b8661785f2d74dfe44a03c82892858bbcf90e6
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-12 15:28:34 +0100

    boolean operators 1

--- src/it/develhope/booleanOperators1/Main.java
+++ src/it/develhope/booleanOperators1/Main.java
@@ -2,10 +2,13 @@ package it.develhope.booleanOperators1;
 
 public class Main {
     public static void main(String[] args) {
-        boolean a = 2 <= 2 && !true; //false
-        boolean b = !false && 3 > 2; //true
-        boolean c = !(!true || false); //true
-        boolean d = 6 > 6 ^ !(true && true); //false
+        boolean a = 2 <= 2 && !true; //FALSE perché 2<=2 è true, mentre !true è false, quindi non sono entrambi true e
+        // l'operatore AND riporterà false
+        boolean b = !false && 3 > 2; //TRUE perché !false è true, così come 3>2, quindi l'operatore AND riporterà true
+        boolean c = !(!true || false); //TRUE perché !true è false, quindi il risultato sarebbe true, ma cambiandolo con
+        // l'operatore NOT sarà comunque true
+        boolean d = 6 > 6 ^ !(true && true); //FALSE perché 6>6 è false, mentre utilizzando NOT su true&&true si otterrà
+        // false, quindi avendo due risultati uguali per l'operatore XOR sarà false
 
         System.out.println(a);
         System.out.println(b);
